b93a9c1b554dd443f933277dd437d69f40b638e6
yamu@123 <mryamuna436@gmail.com>
2024-11-13 13:12:59 +0530

    yamu file moved  into central repository

---
+++ yamu.java
@@ -0,0 +1,6 @@
+class yamu 
+{  Scanner sc=new Scanner(System.in);
+    System.out.println("enters the you are name"); 
+    String name=sc.next(); 
+    System.out.println("you are name Is "+name);
+}
